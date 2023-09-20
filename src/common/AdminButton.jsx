c538d11e993047498f76316862c0815e460368e0
flormartinez92 <martinez.mf.92@gmail.com>
2023-09-20 17:51:45 -0300

    create admin panel buttons

--- src/common/AdminButton.jsx
+++ src/common/AdminButton.jsx
@@ -1,8 +1,20 @@
-export default function AdminButton({ icon, title }) {
+// import Image from "next/image";
+
+export default function AdminButton({ icon, title, className }) {
   return (
-    <button className="w-236 h-224 bg-[#E21B7B]">
-      <icon>{icon}</icon>
-      <h1 className="text-[25px]">{title}</h1>
-    </button>
+    <div className="flex flex-col items-center justify-center mx-auto">
+      <button className="p-[25px] mx-12 my-4 w-236 h-224 bg-[#E21B7B] rounded-[10px] ">
+        <div className="flex flex-col items-center justify-center">
+          {/* <Image
+            src={"/svg/sharp-menu-book.svg"}
+            width={100}
+            height={100}
+            className="justify-center items-center py-[19px]"
+          /> */}
+          <icon className={`${className}`}>{icon}</icon>
+          <p className={`text-[25px] text-white ${className}`}>{title}</p>
+        </div>
+      </button>
+    </div>
   );
 }
