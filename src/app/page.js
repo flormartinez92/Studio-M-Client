7d7d8afd1e62818f86fef6ca833a192e2fe8b6b8
Victoria Canclini <vico_canclini@hotmail.com>
2023-09-20 16:18:06 -0300

    box image

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,16 @@
+import Image from "next/image";
+import Box from "../common/Box";
 export default function Home() {
   return (
     <main>
       <h1 className="font-mystery-mixed text-7xl">Hola Mundo!</h1>
+      <Image
+        src="/public/svg/indonesia.png"
+        width={500}
+        height={500}
+        alt="Picture"
+      />
+      <Box text="Hola Victoria" />
     </main>
   );
 }
