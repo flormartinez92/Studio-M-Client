e9fdfe2d75cd352badf245179b3eec67724b2f47
Victoria Canclini <vico_canclini@hotmail.com>
2023-09-20 16:45:31 -0300

    The common button is separated

--- src/common/Button.jsx
+++ src/common/Button.jsx
@@ -1,9 +1,11 @@
-export default function Button({ label }) {
+export default function Button({ text, className }) {
   return (
-    <div className="p-4 border-4 border-solid border-[#E21B7B] inline-block bg-transparent">
-      <button className="bg-[#1E1E1E] text-[#FFFFFF] font-mystery-mixed text-2xl p-4 w-220 h-81">
-        {label}
-      </button>
-    </div>
+    <button
+      className={`bg-[#1E1E1E] text-[#FFFFFF] font-mystery-mixed text-2xl p-4 w-220 h-81 ${
+        className || ""
+      }`}
+    >
+      {text}
+    </button>
   );
 }
