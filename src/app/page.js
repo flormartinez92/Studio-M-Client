a09288e79ce7557e97fb762c47f3305e5216ac9e
Rafael Mojica <90728588+RafaMojica@users.noreply.github.com>
2023-09-21 11:58:24 -0500

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,7 @@
-import Cards from "../components/Cards";
-
 export default function Home() {
   return (
     <main>
       <h1 className="font-mystery-mixed text-7xl">Hola Mundo!</h1>
-      <Cards title="UX Research" buttonTitle="Ver curso" />
     </main>
   );
 }
