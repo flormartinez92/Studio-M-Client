d7640ea4c95d97bb21ddde2a19756da1a19b421e
Victoria Canclini <vico_canclini@hotmail.com>
2023-09-21 15:18:21 -0300

    Add prop className

--- src/common/CheckList.jsx
+++ src/common/CheckList.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import Border from "./Border";
 import { Check } from "@/common/Icons";
 
-export default function CheckList({ icon }) {
+export default function CheckList({ icon, className }) {
   return (
     <div className="w-80 mx-auto flex justify-center items-center">
-      <Border className="w-20 border-4 py-1 px-2">
+      <Border className={`w-20 border-4 py-1 px-2  ${className || ""}`}>
         <Check /> {icon}
       </Border>
     </div>
