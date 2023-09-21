255b32510094702fe096082d197c2950a307a9b4
Nicolas Fridman <nicofri2001@gmail.com>
2023-09-21 16:20:25 -0300

    CourseInfo.jsx created

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
+import CourseInfo from "@/components/CourseInfo";
+
 export default function Home() {
   return (
     <main>
-      <h1 className="font-mystery-mixed text-7xl">Hola Mundo!</h1>
+      {/* <h1 className="font-mystery-mixed text-7xl">Hola Mundo!</h1> */}
+      <CourseInfo/>
     </main>
   );
 }
