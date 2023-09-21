b74ca7d184bad08f762f2effd838ddd8ed5b5979
Victoria Canclini <vico_canclini@hotmail.com>
2023-09-21 13:24:08 -0300

    finished Card version2

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,20 +2,17 @@ import React from "react";
 import Image from "next/image";
 import Button from "@/common/Button";
 import Border from "../common/Border";
-import { CartShopSimple } from "@/common/Icons";
 
-export default function Cards({ title, buttonTitle }) {
+export default function Cards({ title, buttonTitle, icon, img, className }) {
   return (
-    <div className="w-80 relative">
-      <h2 className="text-4xl text-[#FFFFFF] bg-[#1E1E1E] font-mystery-mixed p-2 flex items-center justify-center rounded-t-md">
+    <div className={`w-80 relative ${className || ""}`}>
+      <h2 className="text-4xl text-white bg-black font-mystery-mixed p-2 flex items-center justify-center rounded-t-md">
         {title}
       </h2>
-      <Image src="/svg/indonesia.png" width={500} height={500} alt="Picture" />
-      <Border className="flex gap-2 m-auto w-60 absolute bottom-2 left-1/2 transform -translate-x-1/2">
-        <Button className="font-mystery-mixed m-auto">{buttonTitle}</Button>
-        <Button>
-          <CartShopSimple />
-        </Button>
+      <Image src={img} width={500} height={500} alt="Picture" />
+      <Border className="flex gap-2 w-[70%] absolute bottom-2 left-1/2 transform -translate-x-1/2">
+        <Button className="font-mystery-mixed">{buttonTitle}</Button>
+        <Button>{icon}</Button>
       </Border>
     </div>
   );
