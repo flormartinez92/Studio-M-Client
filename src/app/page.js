ac3d124048d54da599dd5c972166b1aba17f192a
Victoria Canclini <vico_canclini@hotmail.com>
2023-09-21 23:01:11 -0300

    modify page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,9 @@
 import Navbar from "@/components/Navbar";
+
 export default function Home() {
   return (
-    <main>{<h1 className="font-mystery-mixed text-7xl">Hola Mundo!</h1>}</main>
+    <main>
+      <h1 className="font-mystery-mixed text-7xl">Hola Mundo!</h1>
+    </main>
   );
 }
