6ab59e11b2d1f33001b4e6883080768b736d159a
marcos <marcossolis023@gmail.com>
2023-09-22 15:47:47 -0300

    fixed component My Account

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,9 @@
-import MiAccount from "@/components/MyAccount";
+
 
 export default function Home() {
   return (
     <main>
-      <MiAccount />
-      {/* <h1 className="font-mystery-mixed text-7xl">Hola Mundo!</h1> */}
+      <h1 className="font-mystery-mixed text-7xl">Hola Mundo!</h1>
     </main>
   );
 }
