ebf67929ee7231308a71b59d41ec4e723045c24b
flormartinez92 <martinez.mf.92@gmail.com>
2023-09-22 09:14:28 -0300

    page.js file saved unchanged, as original

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,7 @@
-import PurchasedCourse from "@/common/PurchasedCourse";
-
 export default function Home() {
   return (
     <main>
-      {/* <h1 className="font-mystery-mixed text-7xl">Hola Mundo!</h1> */}
-      <PurchasedCourse/>
+      <h1 className="font-mystery-mixed text-7xl">Hola Mundo!</h1>
     </main>
   );
 }
