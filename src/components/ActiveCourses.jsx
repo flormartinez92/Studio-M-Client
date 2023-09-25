f282bdb3db3324e8d326982a66f9f7e1a49be3b4
Victoria Canclini <vico_canclini@hotmail.com>
2023-09-25 16:47:51 -0300

    changes Active Courses

--- src/components/ActiveCourses.jsx
+++ src/components/ActiveCourses.jsx
@@ -1,44 +1,44 @@
 import Button from "@/common/Button";
-import IconButton from "@/common/IconButton";
 import { Trash, Pencil, Plus, ArrowReload } from "@/common/Icons";
 import ListItem from "@/common/ListItem";
 import React from "react";
 
 export default function ActiveCourses() {
   return (
-    <div>
+    <div className="px-4 md:px-0">
       <h2 className="text-4xl font-mystery-mixed mt-20 mb-10 text-center flex justify-center">
         Cursos Activos
       </h2>
-
-      <ListItem
-        text="UX Writing"
-        iconOne={<Plus color="#4FE21B" />}
-        iconTwo={<Pencil color="#1BBEE2" />}
-        iconThree={<Trash color="#A31616" />}
-        className="w-[319px] h-[40px] ml-5"
-      />
-      <ListItem
-        text="UX Research"
-        iconOne={<Plus color="#4FE21B" />}
-        iconTwo={<Pencil color="#1BBEE2" />}
-        iconThree={<Trash color="#A31616" />}
-        className="w-[319px] h-[40px] ml-5"
-      />
-      <ListItem
-        text="UI Desing"
-        iconOne={<Plus color="#4FE21B" />}
-        iconTwo={<Pencil color="#1BBEE2" />}
-        iconThree={<ArrowReload color="#E21B7B" />}
-        className="w-[319px] h-[40px] ml-5"
-      />
-      <div className="flex justify-end mt-6 mr-24">
-        <Button className="w-[120px] h-[40px] bg-[#389817] flex items-center rounded-md">
-          <Button className=" bg-[#389817] rounded-md p-1">
-            {<Plus className="text-white" />}
+      <div className="md:space-y-0 md:flex md:justify-start">
+        <ListItem
+          text="UX Writing"
+          iconOne={<Plus color="#4FE21B" />}
+          iconTwo={<Pencil color="#1BBEE2" />}
+          iconThree={<Trash color="#A31616" />}
+          className="w-full md:w-[319px] h-[40px]"
+        />
+        <ListItem
+          text="UX Research"
+          iconOne={<Plus color="#4FE21B" />}
+          iconTwo={<Pencil color="#1BBEE2" />}
+          iconThree={<Trash color="#A31616" />}
+          className="w-full md:w-[319px] h-[40px]"
+        />
+        <ListItem
+          text="UI Design"
+          iconOne={<Plus color="#4FE21B" />}
+          iconTwo={<Pencil color="#1BBEE2" />}
+          iconThree={<ArrowReload color="#E21B7B" />}
+          className="w-full md:w-[319px] h-[40px]"
+        />
+        <div className="flex justify-end mt-6 mr-24">
+          <Button className="w-[120px] h-[40px] bg-[#389817] flex items-center rounded-md">
+            <Button className=" bg-[#389817] rounded-md p-1">
+              {<Plus className="text-white" />}
+            </Button>
+            <span className="text-white">Crear curso</span>
           </Button>
-          <span className="text-white">Crear curso</span>
-        </Button>
+        </div>
       </div>
     </div>
   );
