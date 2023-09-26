f0a943284558e5ddbc9ffcf7edd7701b16f8f3a5
Victoria Canclini <vico_canclini@hotmail.com>
2023-09-26 18:38:41 -0300

    new changes-responsive

--- src/common/ListItem.jsx
+++ src/common/ListItem.jsx
@@ -3,15 +3,21 @@ import IconButton from "./IconButton";
 
 export default function ListItem({
   text,
+  text1,
+  text2,
+  text3,
+  text4,
+  text5,
   className,
   iconOne,
   iconTwo,
   iconThree,
   classNameText,
+  classNameP,
 }) {
   return (
     <div
-      className={`flex items-center justify-between bg-white px-4 border-solid border border-gray shadow-lg ${
+      className={`flex items-center justify-between bg-white bg-opacity-0 px-4 border-solid  ${
         className || ""
       }`}
     >
@@ -23,6 +29,11 @@ export default function ListItem({
         {text}
       </p>
       <div className="flex justify-center items-center gap-3">
+        <p className={`hidden ${classNameP}`}>{text1}</p>
+        <p className={`hidden ${classNameP}`}>{text2}</p>
+        <p className={`hidden ${classNameP}`}>{text3}</p>
+        <p className={`hidden ${classNameP}`}>{text4}</p>
+        <p className={`hidden ${classNameP}`}>{text5}</p>
         <IconButton>{iconOne}</IconButton>
         <IconButton>{iconTwo}</IconButton>
         <IconButton>{iconThree}</IconButton>
--- src/components/ActiveCourses.jsx
+++ src/components/ActiveCourses.jsx
@@ -5,31 +5,42 @@ import React from "react";
 
 export default function ActiveCourses() {
   return (
-    <div className="px-4 md:px-0">
-      <h2 className="text-4xl font-mystery-mixed mt-20 mb-10 text-center flex justify-center">
+    <div className="px-4">
+      <h2 className="text-4xl md:text-6xl font-mystery-mixed mt-20 mb-10 md:mb-20 text-center flex justify-center">
         Cursos Activos
       </h2>
-      <div className="md:space-y-0 md:flex md:justify-start">
+      <div className="">
+        <ListItem
+          text="Curso"
+          text1="clases"
+          text2="Alumnos"
+          text3="Agregar"
+          text4="Editar"
+          text5="Bloquear/Habilitar"
+          classNameP="md:block"
+          className="w-full md:w-[1211px] h-[48px] hidden md:block"
+        />
         <ListItem
           text="UX Writing"
+          classNameText="Curso"
           iconOne={<Plus color="#4FE21B" />}
           iconTwo={<Pencil color="#1BBEE2" />}
           iconThree={<Trash color="#A31616" />}
-          className="w-full md:w-[319px] h-[40px]"
+          className="w-full md:w-[1211px] h-[48px] border-b-[0.5px] border-gray"
         />
         <ListItem
           text="UX Research"
           iconOne={<Plus color="#4FE21B" />}
           iconTwo={<Pencil color="#1BBEE2" />}
           iconThree={<Trash color="#A31616" />}
-          className="w-full md:w-[319px] h-[40px]"
+          className="w-full md:w-[1211px] h-[48px] border-b-[0.5px] border-gray"
         />
         <ListItem
           text="UI Design"
           iconOne={<Plus color="#4FE21B" />}
           iconTwo={<Pencil color="#1BBEE2" />}
           iconThree={<ArrowReload color="#E21B7B" />}
-          className="w-full md:w-[319px] h-[40px]"
+          className="w-full md:w-[1211px] h-[48px] shadow-xl "
         />
         <div className="flex justify-end mt-6 mr-24">
           <Button className="w-[120px] h-[40px] bg-[#389817] flex items-center rounded-md">
