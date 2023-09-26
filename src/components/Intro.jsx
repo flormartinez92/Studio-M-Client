706cec0485241b245e44b4c007ff7ba8992e1d67
Victoria Canclini <vico_canclini@hotmail.com>
2023-09-26 12:20:35 -0300

    finished responsive intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,21 +6,21 @@ import Image from "next/image";
 
 export default function Intro() {
   return (
-    <div className="bg-white">
-      <h2 className="text-5xl text-black text-center font-mystery-mixed p-2 mt-40 mb-20 -rotate-6">
+    <div className="bg-white ">
+      <h2 className="text-5xl text-left font-mystery-mixed mt-20 mb-20 md:ml-20 -rotate-3">
         Qué vas a aprender hoy?
       </h2>
-      <div className="flex overflow-x-auto">
-        <div className="w-70 ml-6 mr-4">
+      <div className="flex overflow-x-auto md:bg-[url('/img/paper-desktop-cover.png')] md:bg-[length:100%_500px] md:bg-center md:h-[500px] md:justify-center items-center mb:justify-start">
+        <div className="w-70 ml-6 mr-4 md:w-72 md:ml-6 md:mr-6">
           <Cards
             title="UX Research"
             buttonTitle="Ver curso"
             icon={<CartShopSimple />}
             img="/img/indonesiaGrande.png"
-            className="max-w-[205px] "
+            className="max-w-[205px]"
           />
         </div>
-        <div className="w-70 ml-4 mr-4">
+        <div className="w-70 ml-4 mr-4 md:w-72 md:ml-6 md:mr-6">
           <Cards
             title="UX Writing"
             buttonTitle="Ver curso"
@@ -29,7 +29,7 @@ export default function Intro() {
             className="max-w-[205px]"
           />
         </div>
-        <div className="w-70 ml-4 mr-4">
+        <div className="w-70 ml-4 mr-4 md:w-72 md:ml-6 md:mr-6">
           <Cards
             title="UI Design"
             buttonTitle="Ver curso"
@@ -41,10 +41,10 @@ export default function Intro() {
       </div>
 
       <div className="w-full h-full flex flex-col justify-center">
-        <h2 className="text-5xl text-black text-center font-mystery-mixed p-2 mt-20 mb-20 -rotate-6">
+        <h2 className="text-5xl text-left font-mystery-mixed p-2 mt-20 mb-20 -rotate-3 md:ml-20">
           Qué esperar de un curso en by M studio?
         </h2>
-        <div className="gap-3 flex flex-col items-center justify-center w-[90%]">
+        <div className="md:text-4xl mb:gap-3 flex flex-col items-center justify-center w-[90%] md:ml-40">
           <CheckList
             text="Aprende a tu ritmo y de manera asincrónica"
             className="p-0.5 border-pink mr-5"
@@ -68,16 +68,40 @@ export default function Intro() {
         </div>
       </div>
 
-      <div className="flex flex-col items-center w-100% relative mt-10 mb-20">
-        <Image src="/img/paper.png" width={500} height={500} alt="Picture" />
-        <p className="text-5xl text-black font-mystery-mixed absolute top-10">
-          Studio by M
-        </p>
-        <p className="text-3xl text-black font-mystery-mixed absolute top-28">
-          Cuenta con el apoyo de
-        </p>
-        <Image src="/img/image1.png" width={178} height={87} alt="Picture" />
-        <Image src="/img/image2.png" width={116} height={134} alt="Picture" />
+      <div className="flex overflow-x-auto md:bg-[url('/img/paper-desktop-cover.png')] md:bg-[length:100%_500px] md:bg-center md:h-[500px] md:justify-center items-center mb:justify-start">
+        <div className="flex flex-col items-center w-100% relative mt-10 mb-20">
+          <div className="md:hidden block">
+            <Image
+              src="/img/paper.png"
+              width={500}
+              height={500}
+              alt="Picture"
+            />
+          </div>
+          <p className="text-5xl md:text-8xl whitespace-nowrap font-mystery-mixed absolute top-10 md:top-auto">
+            By M Studio
+          </p>
+          <p className="text-3xl md:text-6xl whitespace-nowrap font-mystery-mixed absolute top-28 md:top-30">
+            Cuenta con el apoyo de
+          </p>
+
+          <div className="md:flex md:justify-center md:top-auto ">
+            <Image
+              src="/img/image1.png"
+              width={178}
+              height={87}
+              alt="Picture"
+              className="md:w-80 md:mt-40"
+            />
+            <Image
+              src="/img/image2.png"
+              width={116}
+              height={134}
+              alt="Picture"
+              className="md:w-32 md:mt-40"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
