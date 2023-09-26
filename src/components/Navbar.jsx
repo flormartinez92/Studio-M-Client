585a946062994749e072164e8ebf7fd9d8f3cfc2
marcos <marcossolis023@gmail.com>
2023-09-26 15:45:23 -0300

    navbar terminada

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@
 
 import IconButton from "@/common/IconButton";
 import { BurgerMenu, CartShopSimple, Close } from "@/common/Icons";
+import Image from "next/image";
 import React, { useState } from "react";
 
 export default function Navbar() {
@@ -14,18 +15,18 @@ export default function Navbar() {
   return (
     <>
       {menuOpen ? (
-        <nav className=" h-screen bg-[url(../../public/img/background.png)] bg-no-repeat bg-cover bg-center">
+        <nav className="h-screen bg-[url(../../public/img/background.png)] bg-no-repeat bg-cover bg-center">
           <div className="flex items-center justify-end">
             <IconButton
-              className="cursor-pointer mx-2 mt-2"
+              className="cursor-pointer mx-4 mt-4"
               onClick={toggleMenu}
             >
               <Close width="24" height="24" color="white" />
             </IconButton>
           </div>
-          <div className="">
-            <ul className="z-10 flex flex-col items-center justify-center absolute w-full left-0 py-7 pl-7 transition-all ease-in duration-500 md:z-auto md:static md:w-auto md:py-0 md:pl-0 ">
-              <li className="">
+          <div className="flex flex-col items-center justify-center z-10 absolute w-full left-0 py-7 pl-7 transition-all ease-in duration-500">
+            <ul className=" md:z-auto md:static md:w-auto md:py-0 md:pl-0 ">
+              <li className="mx-[3rem]">
                 <a
                   href="#"
                   className="text-[50px] text-white font-mystery-mixed hover:text-cyan-500 duration-500"
@@ -33,7 +34,7 @@ export default function Navbar() {
                   Incio
                 </a>
               </li>
-              <li className="">
+              <li className="mx-[2rem]">
                 <a
                   href="#"
                   className="text-[50px] text-white font-mystery-mixed hover:text-cyan-500 duration-500"
@@ -49,18 +50,42 @@ export default function Navbar() {
                   Mi cuenta
                 </a>
               </li>
-              <li className="">
+              <li className="mx-[4.5rem]">
                 <IconButton>
                   <CartShopSimple width="43" height="40" />
                 </IconButton>
               </li>
             </ul>
+
+            <div className=" mx-auto  mb-[60px] mt-[4rem] relative">
+              <Image
+                src="/img/SquarePink.png" //cambiar imagen
+                width={55}
+                height={55}
+                alt="pinkSquare"
+                className="m-auto absolute left-[-1rem] -top-2 "
+              />
+              <Image
+                src="/img/SquareGreen.png" //cambiar imagen
+                width={59}
+                height={59}
+                alt="greenSquare"
+                className="m-auto absolute right-[-1rem] -bottom-3.5 "
+              />
+              <Image
+                src="/img/Maca.png"
+                width={130}
+                height={154}
+                alt="MacaLogo"
+                className="m-auto relative"
+              />
+            </div>
           </div>
         </nav>
       ) : (
-        <nav className=" bg-black p-5 shadow text-white font-mystery-mixed md:flex md:items-center md:justify-between">
+        <nav className=" bg-black p-5 shadow text-white font-mystery-mixed  md:flex md:items-center md:justify-end">
           <div className="flex justify-between items-center">
-            <span className="text-[35px] font-bold cursor-pointer">
+            <span className="text-[35px] font-bold cursor-pointer md:hidden">
               Studio by M
             </span>
             <input
@@ -69,21 +94,46 @@ export default function Navbar() {
               className="hidden md:hidden"
             />
             <label htmlFor="menu-toggle" className="md:hidden">
-              <IconButton className="cursor-pointer mx-2" onClick={toggleMenu}>
+              <IconButton className="cursor-pointer mx-2 mb-2" onClick={toggleMenu}>
                 <BurgerMenu width="24" height="24" />
               </IconButton>
             </label>
+            <div className="hidden md:block">
+              <ul className="md:w-full md:flex md:flex-row md:items-center">
+                <li className="mx-4">
+                  <a
+                    href="#"
+                    className="text-[40px] text-white font-mystery-mixed hover:underline hover:decoration-pink"
+                  >
+                    Incio
+                  </a>
+                </li>
+                <li className="mx-4">
+                  <a
+                    href="#"
+                    className="text-[40px] text-white font-mystery-mixed hover:underline hover:decoration-pink"
+                  >
+                    Cursos
+                  </a>
+                </li>
+                <li className="mx-4">
+                  <a
+                    href="#"
+                    className="text-[40px] text-white font-mystery-mixed hover:underline hover:decoration-pink"
+                  >
+                    Mi cuenta
+                  </a>
+                </li>
+                <li className="mx-4">
+                  <IconButton className="hover:underline hover:decoration-pink">
+                    <CartShopSimple width="43" height="40" />
+                  </IconButton>
+                </li>
+              </ul>
+            </div>
           </div>
         </nav>
       )}
     </>
   );
 }
-
-/*
- className={`md:flex md:items-center z-10 md:z-auto md:static absolute w-full left-0 md:w-auto md:py-0 py-7 md:pl-0 pl-7 transition-all ease-in duration-500 ${
-                menuOpen
-                  ? "transform md:translate-y-0 translate-y-[calc(-100% + 2rem)] opacity-100 md:translate-x-0 translate-x-0"
-                  : "transform md:translate-y-0 translate-y-[calc(-100% + 2rem)] opacity-0 md:opacity-100 md:translate-x-0 translate-x-[2rem]"
-              }`}
-*/
