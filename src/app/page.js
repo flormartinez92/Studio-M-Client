9d856aa1eda20856feaa246f5eaf855927271067
flormartinez92 <martinez.mf.92@gmail.com>
2023-09-27 11:30:27 -0300

    page file modified, just with Cover

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
-import PurchasedCourseDetails from "@/components/PurchasedCourseDetails";
+import Cover from "@/components/Cover";
 
 export default function Home() {
   return (
     <main>
-      <PurchasedCourseDetails />
+      <Cover />
     </main>
   );
 }
