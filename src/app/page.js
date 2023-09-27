dba516327ec910d67bd59182ab1dfcb72bf15b23
Nicolas Fridman <nicofri2001@gmail.com>
2023-09-27 12:24:36 -0300

    layout and page

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,11 @@
-import Courses from "@/components/Courses";
 import Cover from "@/components/Cover";
 import Intro from "@/components/Intro";
 
 export default function Home() {
   return (
     <main>
-      {/* <Cover /> */}
-      {/* <Intro /> */}
-      <Courses/>
+      <Cover />
+      <Intro />
     </main>
   );
 }
