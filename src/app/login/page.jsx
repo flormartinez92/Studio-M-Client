dbeb0322f8b529f4816617a24a07e0f3cf0e11ee
Victoria Canclini <vico_canclini@hotmail.com>
2023-10-02 22:14:24 -0300

    finished login

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,13 +1,51 @@
+"use client";
+
 import Button from "@/common/Button";
 import Input from "@/common/Input";
+import { useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
 export default function Login() {
+  const router = useRouter();
+  const [emailData, setEmailData] = useState("");
+  const [passwordData, setPasswordData] = useState("");
+
+  const handleEmailChange = (e) => {
+    const value = e.target.value;
+    setEmailData(value);
+  };
+
+  const handlePasswordChange = (e) => {
+    const value = e.target.value;
+    setPasswordData(value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios
+      .post("http://localhost:8081/api/user/login", {
+        mail: emailData,
+        password: passwordData,
+      })
+      .then((res) => res.data)
+      .then((user) => {
+        router.push("/");
+        console.log("Successful login:", user);
+      })
+      .catch((error) => {
+        console.error("Unsuccessful login:", error);
+      });
+  };
+
   return (
     <div className="flex flex-col justify-center items-center bg-[#fff] py-[115px]">
       <h2 className="font-mystery-mixed text-[35px] min-[320px]:text-[45px] sm:text-[55px] leading-3">
         Iniciar sesión
       </h2>
       <form
+        onSubmit={handleSubmit}
         className="
             mt-[50px] 
             w-[80%] 
@@ -32,6 +70,8 @@ export default function Login() {
             className={"w-full sm:max-w-[85%]"}
             classNameInput={`p-[5px] outline-none w-[100%] h-[40px] rounded-[3px] bg-black/20`}
             placeholder={"ingresa tu mail"}
+            value={emailData.mail}
+            onChange={handleEmailChange}
           />
         </div>
         <div
@@ -51,6 +91,8 @@ export default function Login() {
             className={"w-full sm:max-w-[85%]"}
             classNameInput={`p-[5px] outline-none w-[100%] h-[40px] rounded-[3px] bg-black/20`}
             placeholder={"ingresa tu contraseña"}
+            value={passwordData.password}
+            onChange={handlePasswordChange}
           />
         </div>
 
