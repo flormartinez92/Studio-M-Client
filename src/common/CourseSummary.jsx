03a33dbc22e2b4fa3af758678a02f2b596214519
Nicolas Fridman <nicofri2001@gmail.com>
2023-10-05 11:32:59 -0300

    redux context for Cart created

--- src/common/CourseSummary.jsx
+++ src/common/CourseSummary.jsx
@@ -3,6 +3,39 @@ import IconButton from "./IconButton";
 import { Clock, Signal, Heart, CartShopPlus } from "./Icons";
 
 export default function CourseSummary({ level, hours, price, className }) {
+  
+  // const addToCart = (id) => {
+  //   axios
+  //     .post(`http://localhost:4000/cart/add/${id}/${state.user.id}`)
+  //     .then((user) => {
+  //       setCarrito();
+  //       message.success("Agregado a carrito", 1);
+  //       const userId = user.data.id;
+  //       // console.log(user.data);
+  //       axios
+  //         .get(`http://localhost:4000/admin/users/${userId}`)
+  //         .then((user) => {
+  //           setState((s) => ({ ...s, user: user.data }));
+  //         });
+  //     })
+  //     .catch((err) => {
+  //       console.log(`error al agregar ${id}`);
+  //     });
+  // };
+  
+  // const { isOnCart, addToCart, removeFromCart, user } = useContext(AuthContext);
+
+// const handleAddToCart = () => {
+//   // funcion para agregar a carrito con book.id
+
+//   if (isOnCart(book.id)) {
+//     removeFromCart(book.id);
+//   } else {
+//     addToCart(book.id);
+//   }
+// };
+
+
   return (
     <Border className={`p-5 border-[3px] border-pink flex flex-col justify-center items-center gap-2 ${className || "" }`}>
       <div className="flex justify-center items-center">
@@ -31,3 +64,29 @@ export default function CourseSummary({ level, hours, price, className }) {
     </Border>
   );
 }
+
+
+
+// const removeFromCart = (id) => {
+//   axios
+//     .delete(`http://localhost:4000/cart/remove/${id}/${state.user.id}`)
+//     .then((user) => {
+//       setCarrito();
+//       message.info("Eliminado del carrito");
+//       const userId = user.data.id;
+//       axios
+//         .get(`http://localhost:4000/admin/users/${userId}`)
+//         .then((user) => {
+//           setState((s) => ({ ...s, user: user.data }));
+//         });
+//     });
+// };
+
+// const isOnCart = (bookId) => {
+//   const arrayOfBooksId = state.carrito.books
+//     ? state.carrito.books.map((m) => m.bookId)
+//     : [];
+
+//   // console.log(arrayOfBooksId.includes(bookId));
+//   return arrayOfBooksId.includes(bookId); //booleano
+// };
