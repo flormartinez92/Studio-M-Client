d25eac27eb65757be9d631035d2fa5dd1b2ae4a9
Nicolas Fridman <nicofri2001@gmail.com>
2023-11-01 13:02:34 -0300

    my certificates desktop-style made

--- src/components/MyCertificates.jsx
+++ src/components/MyCertificates.jsx
@@ -69,16 +69,38 @@ const MyCertificates = () => {
             alt="Picture"
             className="mt-3"
           />
-          <div className="absolute pt-8 top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
-            <h2 className="text-2xl font-mystery-mixed">Certificado</h2>
-            <h3 className="text-3xl font-mystery-mixed">Ux Researcher</h3>
-            <p className="text-base font-ms-gothic">Emilia Rodriguez</p>
-            <p className="text-base font-ms-gothic">DNI: 36.363.363</p>
-            <p className="text-sm font-ms-gothic">
-              Ha realizado y completado con éxito su curso en by M Studio,
-              cumpliendo con todos los requisitos académicos exigidos
-            </p>
-            <p className="text-base font-ms-gothic">03 de Agosto de 2023</p>
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center w-[70%]"> {/*pt-8 top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center */}
+            <div className="flex flex-col align-center items-center my-2 gap-y-1">
+              <h2 className="text-[20px] font-mystery-mixed mb-1">Certificado</h2>
+              <h3 className="text-2xl font-mystery-mixed">Ux Researcher</h3>
+              <p className="text-sm font-ms-gothic">Emilia Rodriguez</p>
+              <p className="text-sm font-ms-gothic">DNI: 36.363.363</p>
+              <p className="text-sm font-ms-gothic mb-2 mt-2">
+                Ha realizado y completado con éxito su curso en by M Studio, <br/>
+                cumpliendo con todos los requisitos académicos exigidos
+              </p>
+              <p className="text-sm font-ms-gothic">03 de Agosto de 2023</p>
+              <div className="flex flex-row justify-around align-center items-center w-full">
+                <div>
+                  <h3 className=" font-mystery-mixed text-[18px]">Studio by M</h3>
+                </div>
+                <div className="flex flex-col items-center align-center">
+                  <Image
+                    src="/img/firma.png"
+                    width={100}
+                    height={100}
+                    alt="Signature"
+                    className="w-[35px] h-[60px]"
+                  />
+                  <div className="">
+                    <h4 className=" font-ms-gothic text-xs text-center">
+                      Macarena <br/>
+                      Bernal
+                    </h4>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="absolute top-16 right-10">
             <IconButton className="flex flex-col">
@@ -86,23 +108,6 @@ const MyCertificates = () => {
               <Share />
             </IconButton>
           </div>
-          <div className="absolute bottom-20 left-40 flex">
-            <h3 className=" font-mystery-mixed text-xl">Studio by M</h3>
-          </div>
-          <div className=" absolute bottom-12 right-44">
-            <h3 className=" font-ms-gothic text-sm text-center">
-              <span>Macarena</span>
-              <br />
-              <span>Bernal</span>
-            </h3>
-          </div>
-          <Image
-            src="/img/firma.png"
-            width={100}
-            height={100}
-            alt="Picture"
-            className="w-[40px] h-[70px] absolute right-48 bottom-24"
-          />
         </div>
       </div>
     </>
