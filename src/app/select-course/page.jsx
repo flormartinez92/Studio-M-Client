1909dc5937f475215602453c7ce99a7dbc98ab6f
Victoria Canclini <vico_canclini@hotmail.com>
2023-11-20 15:25:05 -0300

    modified select-course

--- src/app/select-course/page.jsx
+++ src/app/select-course/page.jsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 import Button from "@/common/Button";
 import { Message } from "../../common/Message";
 import Image from "next/image";
-import { Arrow, BurgerMenu, Check } from "@/common/Icons";
+import { Arrow, ArrowBack, BurgerMenu2, Check } from "@/common/Icons";
 import IconButton from "@/common/IconButton";
 
 export default function SelectCourse() {
@@ -16,13 +16,15 @@ export default function SelectCourse() {
       <div className="flex flex-row w-[90%] items-center justify-between mt-4">
         <div className="flex flex-row justify-between gap-6 text-base">
           <IconButton className={""}>
-            <BurgerMenu width="16" height="16" color={"black"} />
+            <BurgerMenu2 width="30" height="20" color={"black"} />
           </IconButton>
           <h2 className="font-ms-gothic text-base items-center">
             Secciones del curso
           </h2>
         </div>
         <IconButton>
+          <ArrowBack color={"black"} />
+          <span style={{ marginRight: "20px" }}></span>
           <Arrow color={"black"} />
         </IconButton>
       </div>
@@ -31,7 +33,7 @@ export default function SelectCourse() {
           item_num="1"
           text={"Planificacion y diseño de roadmaps de investigacion efectiva"}
           className={
-            "text-white text-2xl w-[90%] bg-no-repeat bg-cover bg-center py-14 sm:py-24 md:py-28 md:px-10 md:text-3xl lg:py-40 lg:px-28 lg:text-4xl xl:py-48 xl:text-5xl"
+            "text-white text-2xl w-[90%] bg-[url(../../public/img/background.png)] bg-no-repeat bg-cover bg-center py-14 sm:py-24 md:py-28 md:px-10 md:text-3xl lg:py-40 lg:px-28 lg:text-4xl xl:py-48 xl:text-5xl"
           }
         />
       </div>
@@ -42,17 +44,17 @@ export default function SelectCourse() {
             "flex flex-row justify-center items-center w-56 p-2 mb-14 bg-green xl:p-6 xl:px-10 xl:w-60 xl:text-3xl"
           }
         >
-          <Check color={"black"} />
+          <Check color={"black"} width={"80"} />
         </Button>
       ) : (
         <Button
           type={"rounder"}
           className={
-            "flex flex-row justify-between items-center w-56 p-2 font-ms-gothic text-2xl mb-14 xl:p-6 xl:px-10 xl:w-60 xl:text-3xl"
+            "flex flex-row justify-between items-center w-56 p-2 font-ms-gothic text-2xl mb-14 xl:p-4 xl:px-10 xl:w-60 xl:text-3xl"
           }
           onClick={handleClick}
         >
-          Completar
+          Completar&nbsp;
           <Image
             src={"/img/Ellipse.png"}
             width={"26"}
