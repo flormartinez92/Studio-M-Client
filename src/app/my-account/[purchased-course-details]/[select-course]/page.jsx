452e3e6413038e048fd0361f2b8104f33e11bd4f
Victoria Canclini <vico_canclini@hotmail.com>
2023-11-23 12:41:58 -0300

    falta solucionar las flechitas

--- src/app/my-account/[purchased-course-details]/[select-course]/page.jsx
+++ src/app/my-account/[purchased-course-details]/[select-course]/page.jsx
@@ -34,35 +34,65 @@ export default function SelectCourse({ params }) {
       .catch((error) => {
         console.error("Error getting courses:", error);
       });
-  }, []);
+  }, [_id, courseId, classId]);
 
   // const goToNextClass = () => {
-  //   if (currentClassIndex < getTotalClasses() - 1) {
+  //   const totalClasses = getTotalClasses();
+  //   if (currentClassIndex < totalClasses - 1) {
   //     setCurrentClassIndex(currentClassIndex + 1);
+  //     updateCourses(currentClassIndex + 1);
   //   }
   // };
 
   // const goToPrevClass = () => {
   //   if (currentClassIndex > 0) {
   //     setCurrentClassIndex(currentClassIndex - 1);
+  //     updateCourses(currentClassIndex - 1);
   //   }
+  // };
+
+  // const updateCourses = (index) => {
+  //   const currentClassKey = Object.keys(courses)[index];
+  //   const currentClassId = courses[currentClassKey]._id;
+
+  //   axios
+  //     .get(
+  //       `${process.env.NEXT_PUBLIC_API_URL}/api/user/courseClass/${_id}/${courseId}/${classId}`
+  //     )
+  //     .then((res) => {
+  //       const updatedCourseClass = res.data;
+  //       setCourses((prevCourses) => {
+  //         const updatedCourses = { ...prevCourses };
+  //         updatedCourses[currentClassKey] = updatedCourseClass;
+  //         return updatedCourses;
+  //       });
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error updating course:", error);
+  //     });
   // };
 
   // const getTotalClasses = () => {
-  //   return courses.length;
+  //   return Object.keys(courses).length;
   // };
 
   // const getCurrentClass = () => {
+  //   const totalClasses = getTotalClasses();
+
   //   if (
-  //     courses.length === 0 ||
+  //     totalClasses === 0 ||
   //     currentClassIndex < 0 ||
-  //     currentClassIndex >= courses.length
+  //     currentClassIndex >= totalClasses
   //   ) {
-  //     return null; // Manejar el caso cuando no hay clases o el índice está fuera de rango
+  //     return null;
   //   }
 
-  //   return courses[currentClassIndex];
+  //   const keys = Object.keys(courses);
+  //   const currentClassKey = keys[currentClassIndex];
+  //   return courses[currentClassKey];
   // };
+
+  // const currentClass = getCurrentClass();
 
   return (
     <div className="bg-white bg-opacity-0 flex flex-col justify-between h-auto items-center gap-8">
@@ -75,16 +105,21 @@ export default function SelectCourse({ params }) {
             Secciones del curso
           </h2>
         </div>
-        <IconButton>
-          <ArrowBack color={"black"} />
-          <span style={{ marginRight: "20px" }}></span>
-          <Arrow color={"black"} />
-        </IconButton>
+        <div className="flex flex-row gap-4">
+          <IconButton>
+            {/* <IconButton onClick={goToPrevClass}> */}
+            <ArrowBack color={"black"} />
+          </IconButton>
+          <IconButton>
+            {/* <IconButton onClick={goToNextClass}> */}
+            <Arrow color={"black"} />
+          </IconButton>
+        </div>
       </div>
       <div className="w-full">
         <Message
           key={courses.classId}
-          item_num="1"
+          item_num={currentClassIndex + 1}
           text={courses.classInfo}
           className={
             "text-white text-2xl w-[90%] bg-[url(../../public/img/background.png)] bg-no-repeat bg-cover bg-center py-14 sm:py-24 md:py-28 md:px-10 md:text-3xl lg:py-40 lg:px-28 lg:text-4xl xl:py-48 xl:text-5xl"
@@ -120,146 +155,3 @@ export default function SelectCourse({ params }) {
     </div>
   );
 }
-
-// "use client";
-
-// import React, { useEffect, useState } from "react";
-// import Button from "@/common/Button";
-// import { Message } from "@/common/Message";
-// import Image from "next/image";
-// import { Arrow, ArrowBack, BurgerMenu2, Check } from "@/common/Icons";
-// import IconButton from "@/common/IconButton";
-// import axios from "axios";
-
-// export default function SelectCourse({ params }) {
-//   const classId = params["select-course"];
-//   const [completed, setCompleted] = useState(false);
-//   const [courses, setCourses] = useState([]);
-//   const [currentClassIndex, setCurrentClassIndex] = useState(0);
-//   const handleClick = () => !completed && setCompleted(true);
-
-//   useEffect(() => {
-//     axios
-//       .get(`${process.env.NEXT_PUBLIC_API_URL}/api/adminCourse/all-courses`)
-//       .then((res) => {
-//         const courses = res.data;
-//         console.log(courses);
-//         setCourses(courses);
-//       })
-//       .catch((error) => {
-//         console.error("Error getting courses:", error);
-//       });
-//   }, []);
-
-//   const goToNextClass = () => {
-//     if (currentClassIndex < getTotalClasses() - 1) {
-//       setCurrentClassIndex(currentClassIndex + 1);
-//     }
-//   };
-
-//   const goToPrevClass = () => {
-//     if (currentClassIndex > 0) {
-//       setCurrentClassIndex(currentClassIndex - 1);
-//     }
-//   };
-
-//   const getTotalClasses = () => {
-//     return courses.reduce(
-//       (total, course) =>
-//         total +
-//         course.modules.reduce(
-//           (moduleTotal, module) =>
-//             moduleTotal +
-//             module.topics.reduce(
-//               (topicTotal, topic) => topicTotal + topic.classes.length,
-//               0
-//             ),
-//           0
-//         ),
-//       0
-//     );
-//   };
-
-//   const getCurrentClass = () => {
-//     let currentIndex = currentClassIndex;
-//     for (const course of courses) {
-//       for (const module of course.modules) {
-//         for (const topic of module.topics) {
-//           const classCount = topic.classes.length;
-//           if (currentIndex < classCount) {
-//             return topic.classes[currentIndex];
-//           } else {
-//             currentIndex -= classCount;
-//           }
-//         }
-//       }
-//     }
-//     return null;
-//   };
-
-//   const currentClass = getCurrentClass();
-
-//   return (
-//     <div className="bg-white bg-opacity-0 flex flex-col justify-between h-auto items-center gap-8">
-//       <div className="flex flex-row w-[90%] items-center justify-between mt-4">
-//         <div className="flex flex-row justify-between items-center gap-2 text-base">
-//           <IconButton className={""}>
-//             <BurgerMenu2 width="30" height="30" color={"black"} />
-//           </IconButton>
-//           <h2 className="font-ms-gothic text-base items-center">
-//             Secciones del curso
-//           </h2>
-//         </div>
-//         <div className="flex justify-between">
-//           <IconButton onClick={goToPrevClass} className={"mr-4"}>
-//             <ArrowBack color={"black"} />
-//           </IconButton>
-//           <IconButton
-//             onClick={goToNextClass}
-//             disabled={currentClassIndex === getTotalClasses() - 1}
-//           >
-//             <Arrow color={"black"} />
-//           </IconButton>
-//         </div>
-//       </div>
-//       <div className="w-full">
-//         {currentClass && (
-//           <Message
-//             key={currentClass._id}
-//             item_num={currentClassIndex + 1}
-//             text={currentClass.classInfo}
-//             className={
-//               "text-white text-2xl w-[90%] bg-[url(../../public/img/background.png)] bg-no-repeat bg-cover bg-center py-14 sm:py-24 md:py-28 md:px-10 md:text-3xl lg:py-40 lg:px-28 lg:text-4xl xl:py-48 xl:text-5xl"
-//             }
-//           />
-//         )}
-//       </div>
-//       {completed ? (
-//         <Button
-//           type={"rounder"}
-//           className={
-//             "flex flex-row justify-center items-center w-40 p-2 mb-14 bg-green xl:p-2 xl:px-10 xl:w-80 xl:text-3xl"
-//           }
-//         >
-//           <Check color={"black"} width={"50"} height={"50"} />
-//         </Button>
-//       ) : (
-//         <Button
-//           type={"rounder"}
-//           className={
-//             "flex flex-row justify-between items-center w-56 p-2 font-ms-gothic text-2xl mb-14 xl:p-2 xl:px-10 xl:w-60 xl:text-3xl"
-//           }
-//           onClick={handleClick}
-//         >
-//           Completar&nbsp;
-//           <Image
-//             src={"/img/Ellipse.png"}
-//             width={"26"}
-//             height={"26"}
-//             alt="ellipse"
-//           />
-//         </Button>
-//       )}
-//     </div>
-//   );
-// }
