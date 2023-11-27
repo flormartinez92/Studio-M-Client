20d2f85b039a3fa43c16a44899a598b59a3ecb9b
Victoria Canclini <vico_canclini@hotmail.com>
2023-11-27 11:25:10 -0300

    styles okk

--- src/app/my-account/[purchased-course-details]/[select-course]/page.jsx
+++ src/app/my-account/[purchased-course-details]/[select-course]/page.jsx
@@ -9,6 +9,7 @@ import { Arrow, ArrowBack, BurgerMenu2, Check } from "@/common/Icons";
 import Button from "@/common/Button";
 import Image from "next/image";
 import { Message } from "@/common/Message";
+import Link from "next/link";
 
 export default function SelectCourse({ params }) {
   const courseId = params["purchased-course-details"];
@@ -37,14 +38,18 @@ export default function SelectCourse({ params }) {
 
   return (
     <div className="bg-white bg-opacity-0 flex flex-col justify-between h-auto items-center gap-8">
-      <div className="flex flex-row w-[90%] items-center justify-between mt-4">
-        <div className="flex flex-row justify-between items-center gap-2 text-base">
+      <div className="flex flex-row  items-center justify-between mt-4 min-w-[90%]">
+        <div className="flex flex-row justify-between items-center gap-2 text-base md:text-xl">
           <IconButton className={""}>
-            <BurgerMenu2 width="30" height="30" color={"black"} />
+            <Link href={`/my-account/${courseId}`}>
+              <BurgerMenu2 width="30" height="30" color={"black"} />
+            </Link>
           </IconButton>
-          <h2 className="font-ms-gothic text-base items-center">
-            Secciones del curso
-          </h2>
+          <Link href={`/my-account/${courseId}`}>
+            <h2 className="font-ms-gothic text-base items-center md:text-xl">
+              Secciones del curso
+            </h2>
+          </Link>
         </div>
         <div className="flex flex-row gap-4">
           <IconButton>
@@ -55,15 +60,17 @@ export default function SelectCourse({ params }) {
           </IconButton>
         </div>
       </div>
-      <div className="w-full">
-        <Message
-          key={courses.classId}
-          item_num={currentClassIndex + 1}
-          text={courses.classInfo}
-          className={
-            "text-white text-2xl w-[90%] bg-[url(../../public/img/background.png)] bg-no-repeat bg-cover bg-center py-14 sm:py-24 md:py-28 md:px-10 md:text-3xl lg:py-40 lg:px-28 lg:text-4xl xl:py-48 xl:text-5xl"
-          }
-        />
+      <div className="min-w-[60%]">
+        <Link href={`https://www.youtube.com/`}>
+          <Message
+            key={courses.classId}
+            item_num={currentClassIndex + 1}
+            text={courses.classInfo}
+            className={
+              "text-white text-2xl w-[90%] bg-[url(../../public/img/background.png)] bg-no-repeat bg-cover bg-center py-14 sm:py-24 md:py-28 md:px-10 md:text-3xl lg:py-40 lg:px-28 lg:text-4xl xl:py-48 xl:text-5xl"
+            }
+          />
+        </Link>
       </div>
       {completed ? (
         <Button
@@ -78,7 +85,7 @@ export default function SelectCourse({ params }) {
         <Button
           type={"rounder"}
           className={
-            "flex flex-row justify-between items-center w-56 p-2 font-ms-gothic text-2xl mb-14 xl:p-2 xl:px-10 xl:w-60 xl:text-3xl"
+            "flex flex-row justify-between items-center w-56 p-2 font-ms-gothic text-2xl mb-14 xl:p-2 xl:px-10 xl:w-60 xl:text-3xl mt-6"
           }
           onClick={handleClick}
         >
