3a2edfe41c675213db5a07e3c22677c7a982822e
Victoria Canclini <vico_canclini@hotmail.com>
2023-11-30 12:38:16 -0300

    agregue un modalComment para la vista de proyectos activos

--- src/app/active-projects/page.jsx
+++ src/app/active-projects/page.jsx
@@ -13,10 +13,19 @@ import {
 import axios from "axios";
 import { useState } from "react";
 import { useEffect } from "react";
+import ModalCommentProject from "@/components/ModalCommentProject";
 
 export default function ActiveProjects() {
   const [projects, setProjects] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
+  const [modal, setModal] = useState(false);
+  const [selectedProjectId, setSelectedProjectId] = useState(null);
+  console.log(selectedProjectId);
+
+  const handleProjectClick = (projectId) => {
+    setSelectedProjectId(projectId);
+    setModal(true);
+  };
 
   const projectsPerPage = 10;
   const totalPages = Math.ceil(projects.length / projectsPerPage);
@@ -48,6 +57,7 @@ export default function ActiveProjects() {
       const projects = await axios.get(
         `${process.env.NEXT_PUBLIC_API_URL}/api/adminProject/allProjects`
       );
+      console.log(projects);
       setProjects(projects.data);
     } catch (error) {
       console.error(error);
@@ -100,86 +110,99 @@ export default function ActiveProjects() {
   };
 
   return (
-    <section className="my-20 mb-60">
-      <h2 className="text-4xl md:text-5xl xl:text-6xl font-mystery-mixed mt-10 mb-10 md:mb-15 xl:mb-20 text-center flex justify-center">
-        Proyectos para corregir
-      </h2>
-      <div className="flex justify-center px-4 font-ms-gothic md:ml-10 xl:ml-10 md:mr-10 xl:mr-10 ">
-        <table className="w-full xl:table-fixed">
-          <thead className="max-sm:hidden">
-            <tr className="w-full md:w-[740px] xl:w-[1211px] h-[48px] border-b-[0.5px] md:border-l-[0.5px] border-lightGrey  md:border-r-[0.5px] rounded-t-lg text-[#757575] border-t-[0.05px]">
-              <td className="p-4">Nombre completo</td>
-              <td className="sm:pr-10">Curso</td>
-              <td className="sm:pr-10">Link de entrega</td>
-              <td className="sm:pr-10 md:pr-10">Agregar comentario</td>
-              <td className="sm:pr-10 md:pr-10">Aprobar</td>
-            </tr>
-          </thead>
-          <tbody>
-            {projects?.slice(startIndex, endIndex).map((project) => (
-              <tr
-                key={project.userId}
-                className="w-full md:w-[740px] xl:w-[1211px] h-[48px] border-b-[0.5px] md:border-l-[0.5px] border-lightGrey md:border-r-[0.5px] "
-              >
-                <td className="p-4">{project.name + " " + project.lastname}</td>
-                <td className="max-sm:hidden">{project.courseShortTitle}</td>
-                <td className="pl-10">
-                  <button onClick={() => handleClick(project.project_url)}>
-                    <Link color="#E21B7B" />
+    <>
+      {modal && (
+        <ModalCommentProject
+          status={modal}
+          closeModal={() => setModal(false)}
+          projectId={selectedProjectId}
+        />
+      )}
+      <section className="my-20 mb-60">
+        <h2 className="text-4xl md:text-5xl xl:text-6xl font-mystery-mixed mt-10 mb-10 md:mb-15 xl:mb-20 text-center flex justify-center">
+          Proyectos para corregir
+        </h2>
+        <div className="flex justify-center px-4 font-ms-gothic md:ml-10 xl:ml-10 md:mr-10 xl:mr-10 ">
+          <table className="w-full xl:table-fixed">
+            <thead className="max-sm:hidden">
+              <tr className="w-full md:w-[740px] xl:w-[1211px] h-[48px] border-b-[0.5px] md:border-l-[0.5px] border-lightGrey  md:border-r-[0.5px] rounded-t-lg text-[#757575] border-t-[0.05px]">
+                <td className="p-4">Nombre completo</td>
+                <td className="sm:pr-10">Curso</td>
+                <td className="sm:pr-10">Link de entrega</td>
+                <td className="sm:pr-10 md:pr-10">Agregar comentario</td>
+                <td className="sm:pr-10 md:pr-10">Aprobar</td>
+              </tr>
+            </thead>
+            <tbody>
+              {projects?.slice(startIndex, endIndex).map((project) => (
+                <tr
+                  key={project.userId}
+                  className="w-full md:w-[740px] xl:w-[1211px] h-[48px] border-b-[0.5px] md:border-l-[0.5px] border-lightGrey md:border-r-[0.5px] "
+                >
+                  <td className="p-4">
+                    {project.name + " " + project.lastname}
+                  </td>
+                  <td className="max-sm:hidden">{project.courseShortTitle}</td>
+                  <td className="pl-10">
+                    <button onClick={() => handleClick(project.project_url)}>
+                      <Link color="#E21B7B" />
+                    </button>
+                  </td>
+                  <td className="pl-10">
+                    <button
+                      onClick={() => handleProjectClick(project.projectId)}
+                    >
+                      <Plus color="#4FE21B" />
+                    </button>
+                  </td>
+                  <td className="p-4">
+                    <button onClick={() => showAlert(project.projectId)}>
+                      {project.status ? (
+                        <Check color="#A31616" />
+                      ) : (
+                        <BoxCheck color="#E21B7B" />
+                      )}
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+            <tfoot className="w-full md:w-[740px] xl:w-[1211px] h-[48px] max-sm:hidden border-t-[0.5px] border-lightGrey shadow-xl md:border-r-[0.5px] md:border-l-[0.5px] rounded-b-lg">
+              <tr>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td></td>
+                <td>Filas por página</td>
+                <td className="flex justify-between mt-3">
+                  &nbsp; {currentPage} de {totalPages}
+                  <button
+                    onClick={() =>
+                      setCurrentPage((prevPage) => Math.max(prevPage - 1, 1))
+                    }
+                    disabled={currentPage === 1}
+                  >
+                    <UilArrow1
+                      color={currentPage === 1 ? "lightGrey" : "black"}
+                    />
                   </button>
-                </td>
-                <td className="pl-10">
-                  <button>
-                    <Plus color="#4FE21B" />
-                  </button>
-                </td>
-                <td className="p-4">
-                  <button onClick={() => showAlert(project.projectId)}>
-                    {project.status ? (
-                      <Check color="#A31616" />
-                    ) : (
-                      <BoxCheck color="#E21B7B" />
-                    )}
+                  <button
+                    onClick={() =>
+                      setCurrentPage((prevPage) =>
+                        Math.min(prevPage + 1, totalPages)
+                      )
+                    }
+                    disabled={currentPage === totalPages}
+                  >
+                    <UilArrow2
+                      color={currentPage === totalPages ? "lightGrey" : "black"}
+                    />
                   </button>
                 </td>
               </tr>
-            ))}
-          </tbody>
-          <tfoot className="w-full md:w-[740px] xl:w-[1211px] h-[48px] max-sm:hidden border-t-[0.5px] border-lightGrey shadow-xl md:border-r-[0.5px] md:border-l-[0.5px] rounded-b-lg">
-            <tr>
-              <td>&nbsp;</td>
-              <td>&nbsp;</td>
-              <td></td>
-              <td>Filas por página</td>
-              <td className="flex justify-between mt-3">
-                &nbsp; {currentPage} de {totalPages}
-                <button
-                  onClick={() =>
-                    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1))
-                  }
-                  disabled={currentPage === 1}
-                >
-                  <UilArrow1
-                    color={currentPage === 1 ? "lightGrey" : "black"}
-                  />
-                </button>
-                <button
-                  onClick={() =>
-                    setCurrentPage((prevPage) =>
-                      Math.min(prevPage + 1, totalPages)
-                    )
-                  }
-                  disabled={currentPage === totalPages}
-                >
-                  <UilArrow2
-                    color={currentPage === totalPages ? "lightGrey" : "black"}
-                  />
-                </button>
-              </td>
-            </tr>
-          </tfoot>
-        </table>
-      </div>
-    </section>
+            </tfoot>
+          </table>
+        </div>
+      </section>
+    </>
   );
 }
---
+++ src/components/ModalCommentProject.jsx
@@ -0,0 +1,158 @@
+"use client";
+
+import Border from "@/common/Border";
+import Button from "@/common/Button";
+import { useMediaQuery } from "@react-hook/media-query";
+import axios from "axios";
+import { jwtDecode } from "jwt-decode";
+import { useRef, useEffect, useState } from "react";
+
+export default function ModalCommentProject({ status, closeModal, projectId }) {
+  const dialofRef = useRef(null);
+  const [comment, setComment] = useState("");
+  const [showThanksModal, setShowThanksModal] = useState(false);
+  const isMdBreakpoint = useMediaQuery("(min-width: 768px)");
+  const userToken = sessionStorage.getItem("token");
+  const { mail } = jwtDecode(userToken);
+
+  const handleInputComment = (event) => {
+    event.preventDefault();
+    setComment(event.target.value);
+  };
+
+  const handleModalContentClick = (event) => {
+    event.stopPropagation();
+  };
+
+  const handleButtonClick = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.put(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/adminProject/${projectId}`,
+        { comment, mail }
+      );
+      setShowThanksModal(true);
+    } catch (error) {
+      console.error("Error al enviar el comentario:", error);
+    }
+  };
+
+  const handleThanksModalClose = () => {
+    setShowThanksModal(false);
+    closeModal();
+  };
+
+  useEffect(() => {
+    if (status) {
+      dialofRef.current?.showModal();
+      document.body.style.overflow = "hidden";
+    } else {
+      dialofRef.current?.close();
+      document.body.style.overflow = "";
+    }
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [status]);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        status &&
+        dialofRef.current &&
+        !dialofRef.current.contains(event.target)
+      ) {
+        console.log("Cerrando modal con click por fuera");
+        closeModal();
+      }
+    };
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [status, closeModal]);
+
+  return (
+    <>
+      {status && !showThanksModal && (
+        <div
+          onClick={() => closeModal()}
+          className="fixed z-10 inset-0 flex items-center justify-center bg-black bg-opacity-50"
+        >
+          <div
+            onClick={(event) => handleModalContentClick(event)}
+            className="flex items-center justify-center w-[80%] h-[27%] border-[2px] border-dashed border-purple rounded-md
+        min-[560px]:w-[90%] min-[560px]:h-[35%]
+        md:w-[90%] md:h-[50%]
+        lg:w-[90%] lg:h-[90%]"
+          >
+            <div className="flex flex-col justify-center items-center bg-[url(../../public/img/background.png)] bg-no-repeat bg-cover bg-center w-[97%] h-[97%] md:justify-normal md:pt-5 lg:justify-center min-[820]:justify-center">
+              <h2
+                className=" py-1 font-mystery-mixed text-white text-xl min-[390px]:text-2xl min-[390px]:pb-3
+              min-[560px]:text-4xl md:text-5xl md:pb-7 md:pt-9 min-[820px]:pt-16 lg:text-6xl lg:py-10 xl:text-7xl
+              "
+              >
+                Agregar comentario
+              </h2>
+
+              <form onSubmit={handleButtonClick} className="w-[75%] md:w-[80%]">
+                {isMdBreakpoint ? (
+                  <div className="flex flex-col justify-start items-start pb-2 ">
+                    <h3 className=" font-mystery-mixed text-white text-start text-3xl lg:text-4xl">
+                      Comentario
+                    </h3>
+                  </div>
+                ) : null}
+                <div className="flex flex-col justify-center items-center">
+                  <textarea
+                    onChange={handleInputComment}
+                    type="text"
+                    value={comment}
+                    className=" bg-lightGrey w-[100%] "
+                  ></textarea>
+                </div>
+              </form>
+
+              <div className="pt-2 flex items-center justify-center w-full min-[390px]:pt-4 md:pt-7">
+                <Border
+                  className="flex flex-col justify-center items-center w-[120px] h-[25px] border-[1px] border-pink md:w-[300px] md:h-[55px] lg:h-[60px] 
+                "
+                >
+                  <Button
+                    onClick={handleButtonClick}
+                    className=" w-[95%] h-[80%] text-center text-sm font-mystery-mixed md:text-3xl"
+                  >
+                    {isMdBreakpoint ? "Enviar Comentario" : "Enviar"}
+                  </Button>
+                </Border>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {showThanksModal && (
+        <div
+          onClick={handleThanksModalClose}
+          className="fixed z-10 inset-0 flex items-center justify-center bg-black bg-opacity-50"
+        >
+          <div
+            className="flex items-center justify-center w-[80%] h-[27%] border-[2px] border-dashed border-purple rounded-md
+        min-[560px]:w-[90%] min-[560px]:h-[35%]
+        md:w-[90%] md:h-[40%]
+        lg:w-[90%] lg:h-[90%]"
+          >
+            <div className="flex flex-col justify-center items-center bg-[url(../../public/img/background.png)] bg-no-repeat bg-cover bg-center w-[97%] h-[97%]">
+              <h2 className="py-2 font-mystery-mixed text-white text-lg min-[390px]:text-2xl min-[390px]:pb-12 md:text-5xl lg:text-6xl">
+                ¡Comentario enviado!
+              </h2>
+              <p className="pb-3 font-ms-gothic text-xs font-light text-white text-center w-[80%] leading-3 min-[390px]:text-sm min-[390px]:leading-3  md:text-3xl md:pt-8 lg:text-4xl">
+                Recibirás el comentario del proyecto por email.
+              </p>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
