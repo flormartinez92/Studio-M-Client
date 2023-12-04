7abafbee60d1fa250a40a85cdcd448b63d089535
Victoria Canclini <vico_canclini@hotmail.com>
2023-12-04 15:18:59 -0300

    modifique select-course y puse el video adentro de Message

--- src/app/my-account/[purchased-course-details]/[select-course]/page.jsx
+++ src/app/my-account/[purchased-course-details]/[select-course]/page.jsx
@@ -9,6 +9,7 @@ import { Arrow, ArrowBack, BurgerMenu2, Check } from "@/common/Icons";
 import Button from "@/common/Button";
 import Image from "next/image";
 import Link from "next/link";
+import { Message } from "@/common/Message";
 
 export default function SelectCourse({ params }) {
   const courseId = params["purchased-course-details"];
@@ -19,6 +20,7 @@ export default function SelectCourse({ params }) {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [nextClassId, setNextClassId] = useState("");
   const [previousClassId, setPreviousClassId] = useState("");
+  const [showVideo, setShowVideo] = useState(false);
 
   const userToken = sessionStorage.getItem("token");
   const { _id, mail } = jwtDecode(userToken);
@@ -119,17 +121,31 @@ export default function SelectCourse({ params }) {
         </div>
       </div>
       <div className="w-full m-auto">
-        <div className="w-[90%] h-[600px] text-center mx-auto grid place-items-center">
-          <ReactPlayer
-            width="100%"
-            height="100%"
-            className="bg-black"
-            url={courseClass.video_url}
+        {!showVideo && (
+          <Message
             key={courseClass.classId}
             item_num={currentIndex + 1}
             text={courseClass.classInfo}
+            url={courseClass.video_url}
+            className={
+              "text-white text-2xl w-[90%] bg-[url(../../public/img/background.png)] bg-no-repeat bg-cover bg-center py-14 sm:py-24 md:py-28 md:px-10 md:text-3xl lg:py-40 lg:px-28 lg:text-4xl xl:py-48 xl:text-5xl"
+            }
+            onClick={() => setShowVideo(true)}
           />
-        </div>
+        )}
+        {showVideo && (
+          <div className="w-[90%] h-[200px] sm:w-[90%] sm:h-[300px] md:w-[90%] md:h-[400px] xl:w-[90%] xl:h-[600px] text-center mx-auto grid place-items-center">
+            <ReactPlayer
+              width="100%"
+              height="100%"
+              className="bg-black"
+              url={courseClass.video_url}
+              key={courseClass.classId}
+              item_num={currentIndex + 1}
+              text={courseClass.classInfo}
+            />
+          </div>
+        )}
       </div>
       {completed ? (
         <Button
@@ -144,7 +160,7 @@ export default function SelectCourse({ params }) {
         <Button
           type={"rounder"}
           className={
-            "flex flex-row justify-between items-center w-56 p-2 font-ms-gothic text-2xl mb-14 xl:p-2 xl:px-10 xl:w-60 xl:text-3xl mt-6"
+            "flex flex-row justify-between items-center w-56 p-2 font-ms-gothic text-2xl mb-14 xl:p-2 xl:px-10 xl:w-60 xl:text-3xl md:text-3xl mt-6"
           }
           onClick={handleClick}
         >
